a0dc32d2e66f533fa95816a671d6a321363c1fc9
Antonin Kral <antonin.kral@dtone.com>
2019-10-16 20:35:37 +0200

    Endpoint to respond to FB challange

--- lib/workplace2slack/router.ex
+++ lib/workplace2slack/router.ex
@@ -9,6 +9,16 @@ defmodule Workplace2slack.Router do
     send_resp(conn, 200, "OK")
   end
 
+  get "/workplace" do
+    IO.inspect conn.query_params
+    IO.inspect conn.body_params
+    case conn.query_params["hub.challenge"] do
+      nil -> send_resp(conn, 200, "OK")
+      challange when challange > 0 -> send_resp(conn, 200, challange)
+      _ -> send_resp(conn, 200, "OKK")
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "not found")
   end
