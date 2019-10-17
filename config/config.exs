479588c21bf19dbbafc9d3d1b07e01cd4c453cef
Antonin Kral <antonin.kral@dtone.com>
2019-10-17 10:47:24 +0200

    Let's commit some secrets, shall we

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,8 @@ use Mix.Config
 # Configures the endpoint
 config :workplace2slack,
   url: [host: "localhost"],
-  root: Path.dirname(__DIR__)
+  root: Path.dirname(__DIR__),
+  slack_token: "xoxp-582148963089-605486615847-785409687250-6a4d6ec19e5b322c47c50fd1f4b28858"
 
 # Configures Elixir's Logger
 config :logger, :console,
