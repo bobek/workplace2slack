89298a7f82c8543d6d9f56b63efac9b42b0cb5db
Antonin Kral <antonin.kral@dtone.com>
2019-10-17 21:00:23 +0200

    Add request_id into log messages

--- lib/workplace2slack/router.ex
+++ lib/workplace2slack/router.ex
@@ -3,6 +3,7 @@ defmodule Workplace2Slack.Router do
   alias Workplace2Slack.Workplace
 
   plug :match
+  plug Plug.RequestId
   plug Plug.Logger
   plug Plug.Parsers,
     parsers: [:json, :urlencoded],
