2a1d04e57973d429f77bb3d5aa879175b3682397
Antonin Kral <antonin.kral@dtone.com>
2019-10-17 15:51:46 +0200

    Parting attached images

--- lib/workplace2slack/router.ex
+++ lib/workplace2slack/router.ex
@@ -19,15 +19,41 @@ defmodule Workplace2slack.Router do
     end
   end
 
+  def extract_image_url(%{"media" => %{"image" => %{"src" => src}}, "type" => "photo"}), do: [src]
+  def extract_image_url(%{"subattachments" => %{"data" => data}}), do: Enum.flat_map(data, fn x -> extract_image_url(x) end)
+  def extract_image_url(_), do: []
+
+  def truncate(text, opts \\ []) do
+    max_length  = opts[:max_length] || 50
+    omission    = opts[:omission] || "..."
+
+    cond do
+      not String.valid?(text) ->
+        text
+      String.length(text) < max_length ->
+        text
+      true ->
+        length_with_omission = max_length - String.length(omission)
+
+        "#{String.slice(text, 0, length_with_omission)}#{omission}"
+    end
+  end
+
   post "/workplace" do
     IO.puts "Received message from FB"
 
     with %{"entry" => [%{"changes" => [change|_]}|_], "object" => "group"} <- conn.body_params,
-         %{"field" => "posts", "value" => %{"community" => %{"id" => community_id}, "message" => message, "permalink_url" => permalink_url}} <- change do
+         %{"field" => "posts", "value" => %{"community" => %{"id" => _community_id}, "from" => %{"name" => author}, "message" => message, "permalink_url" => permalink_url}} <- change do
 
-      IO.inspect community_id
-      IO.inspect permalink_url
-      IO.inspect message
+      attachment_urls = case change do
+        %{"field" => "posts", "value" => %{ "attachments" => %{"data" => attachments}}} -> Enum.flat_map(attachments, fn x -> extract_image_url(x) end)
+        _ -> []
+      end
+      IO.inspect attachment_urls
+
+      images = attachment_urls
+      |> List.flatten
+      |> Enum.map(fn x -> %{type: "image", image_url: x, alt_text: "image"} end)
 
       slack_msg =
       %{
@@ -37,15 +63,22 @@ defmodule Workplace2slack.Router do
         parse: "full",
         blocks: [
           %{
-              type: "section",
-              text: %{
+            type: "section",
+            text: %{
               type: "mrkdwn",
-              text: message,
-              },
+              text: "New post from #{author} - #{permalink_url}",
+            },
           },
-        ],
+          %{
+            type: "section",
+            text: %{
+              type: "mrkdwn",
+              text: truncate(message, max_length: 1567)
+            },
+           } | images ],
       }
 
+      IO.inspect slack_msg
       {:send_message, [slack_msg]} |> Honeydew.async(:slack)
     end
 
