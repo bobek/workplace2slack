9d053e559afb3a10a3f4e3c2b017af25e48abf0e
Antonin Kral <antonin.kral@dtone.com>
2019-10-17 12:08:30 +0200

    Adding mnesia among extra apps to be included when released
    https://github.com/koudelka/honeydew/issues/94

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Workplace2slack.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :mnesia],
       mod: {Workplace2slack.Application, []}
     ]
   end
