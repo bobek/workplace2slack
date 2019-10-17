e50ac626f8a469b57bc7ad643a562e75315a3e36
Antonin Kral <antonin.kral@dtone.com>
2019-10-17 12:08:47 +0200

    Cleaning up the boot sequence a bit

--- lib/workplace2slack/application.ex
+++ lib/workplace2slack/application.ex
@@ -12,12 +12,14 @@ defmodule Workplace2slack.Application do
       {Plug.Cowboy, scheme: :http, plug: Workplace2slack.Router, options: [port: 4000]},
     ]
 
-    :ok = Honeydew.start_queue(:slack)
-    :ok = Honeydew.start_workers(:slack, {Workplace2slack.SlackWorker, [Application.get_env(:workplace2slack, :slack_token, ""), "CP3C0HB26"]})
-
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Workplace2slack.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, supervisor} = Supervisor.start_link(children, opts)
+
+    :ok = Honeydew.start_queue(:slack)
+    :ok = Honeydew.start_workers(:slack, {Workplace2slack.SlackWorker, [Application.get_env(:workplace2slack, :slack_token, ""), "CP3C0HB26"]})
+
+    {:ok, supervisor}
   end
 end
